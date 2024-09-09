2fb26621da30d8e4163ba319480f731c5ffdf8a7
hakimamarullah <hakimamarullah@gmail.com>
2024-09-09 11:17:16 +0700

    add user info endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UsersService } from './users.service';
+import { UserDto } from './dto/user.dto';
+import { ApiBaseResponse } from '../common/decorators/swagger.decorator';
 
+@ApiBearerAuth()
+@ApiTags('UserController')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get('/:username/details')
+  @ApiOperation({ summary: 'Get user info details by username or email' })
+  @ApiBaseResponse({ model: UserDto })
+  @HttpCode(HttpStatus.OK)
+  @ApiParam({
+    name: 'username',
+    type: String,
+    description: 'username or email',
+  })
+  async getUserInfo(@Param('username') username: string) {
+    return await this.userService.findByUsernameOrEmail(username);
+  }
+}
